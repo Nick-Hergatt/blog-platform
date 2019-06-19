c1bc451f321eab9648793fb5f49f12199a7e48b1
Nick-Hergatt <nickmachergatt@gmail.com>
2019-06-19 15:52:32 -0400

    first commit

---
+++ src/test/java/masteryproject/blogplatform/BlogPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package masteryproject.blogplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogPlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
