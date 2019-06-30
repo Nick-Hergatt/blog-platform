d665099a4eb5bd0a9f85dcae54b2bf0368770155
Nick-Hergatt <nickmachergatt@gmail.com>
2019-06-30 13:47:46 -0400

    more refactoring

--- src/main/java/masteryproject/blogplatform/Category.java
+++ src/main/java/masteryproject/blogplatform/Category.java
@@ -12,7 +12,8 @@ import javax.persistence.OneToMany;
 public class Category {
 
 	@OneToMany(mappedBy = "category")
-	private List<Post> posts;
+	private Collection<Post> posts;
+
 
 	private String name;
 
@@ -43,4 +44,40 @@ public class Category {
 	}
 
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((posts == null) ? 0 : posts.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (posts == null) {
+			if (other.posts != null)
+				return false;
+		} else if (!posts.equals(other.posts))
+			return false;
+		return true;
+	}
 }
